4dd9749ef32d0844373ad62063470204b150f4f7
Shiva Sankar <ms.shiva2019@gmail.com>
2023-11-02 23:00:11 +0530

    combined login and main page modules

--- sample/src/main.jsx
+++ sample/src/main.jsx
@@ -2,9 +2,17 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import {BrowserRouter as Router, Routes, Route, Link, BrowserRouter} from 'react-router-dom'
+import LoginPage from "./components/LoginPage.jsx";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+      <BrowserRouter>
+          <App />
+      </BrowserRouter>
+      {/*<Routes>*/}
+      {/*    <Route path="/signin" element={<LoginPage />} />*/}
+      {/*    <Route path="/home" element={<App />} />*/}
+      {/*</Routes>*/}
+  </React.StrictMode>
 )
